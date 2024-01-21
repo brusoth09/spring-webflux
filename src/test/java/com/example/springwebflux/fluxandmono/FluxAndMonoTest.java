1886d4dc232907c69a32c3fd2037cea4f4918be1
brusoth09 <brusoth09@gmail.com>
2024-01-20 23:34:03 -0500

    More test cases

--- src/test/java/com/example/springwebflux/fluxandmono/FluxAndMonoTest.java
+++ src/test/java/com/example/springwebflux/fluxandmono/FluxAndMonoTest.java
@@ -1,8 +1,10 @@
 package com.example.springwebflux.fluxandmono;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
 
 class FluxAndMonoTest {
     @Test
@@ -15,6 +17,39 @@ class FluxAndMonoTest {
         flux.repeat(2).subscribe(System.out::println,
                 System.err::println,
                 () -> System.out.println("Completed"));
+    }
+
+    @Test
+    void fluxTestElementsWithoutError(){
+        Flux<String> flux = Flux.just("Hello", "Hi", "Hola")
+                .log();
+        StepVerifier.create(flux)
+                .expectNext("Hello")
+                .expectNext("Hi")
+                .expectNext("Hola")
+                .verifyComplete();
+    }
+
+    @Test
+    void fluxTestElementsWithError(){
+        Flux<String> flux = Flux.just("Hello", "Hi", "Hola")
+                .concatWith(Flux.error(new RuntimeException("Error")))
+                .log();
+        StepVerifier.create(flux)
+                .expectNext("Hello")
+                .expectNext("Hi")
+                .expectNext("Hola")
+                .expectErrorMessage("Error")
+                .verify();
+    }
+
+    @Test
+    void fluxTestElementCount(){
+        Flux<String> flux = Flux.just("Hello", "Hi", "Hola")
+                .log();
+        StepVerifier.create(flux)
+                .expectNextCount(3)
+                .verifyComplete();
     }
 
     @Test
