44201c48a79d54be1e26a903a80d93309c0afb7f
brusoth09 <brusoth09@gmail.com>
2024-01-21 11:29:28 -0500

    more test cases

--- src/test/java/com/example/springwebflux/controller/FluxControllerTest.java
+++ src/test/java/com/example/springwebflux/controller/FluxControllerTest.java
@@ -6,9 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.EntityExchangeResult;
 import org.springframework.test.web.reactive.server.WebTestClient;
 import reactor.core.publisher.Flux;
 import reactor.test.StepVerifier;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 @RunWith(SpringRunner.class)
 @WebFluxTest
@@ -31,5 +36,44 @@ public class FluxControllerTest {
                 .expectNext(2)
                 .expectNext(3)
                 .verifyComplete();
+    }
+
+    @Test
+    public void fluxTestHeader() {
+        webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                        .hasSize(3);
+    }
+
+    @Test
+    public void fluxTestAnotherApproach() {
+        List<Integer> expected = List.of(1, 2, 3);
+        EntityExchangeResult<List<Integer>> result = webTestClient.get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .returnResult();
+        assertEquals(expected, result.getResponseBody());
+    }
+
+    @Test
+    public void fluxTestConsumerApproach() {
+        List<Integer> expected = List.of(1, 2, 3);
+        webTestClient
+                .get().uri("/flux")
+                .accept(MediaType.APPLICATION_JSON_UTF8)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().contentType(MediaType.APPLICATION_JSON_UTF8)
+                .expectBodyList(Integer.class)
+                .consumeWith((res) -> {
+                    assertEquals(expected, res.getResponseBody());
+                });
     }
 }
